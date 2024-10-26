71a79eb037f78e23f32b7e670ec0da60229e1d9b
Emilia Bopp <contact@ebopp.de>
2024-10-26 18:38:53 +0200

    Implement add

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/add/<int:a>/<int:b>")
+def add(a, b):
+    return str(a + b)
+
 app.run(port=os.getenv("PORT", 8080))
